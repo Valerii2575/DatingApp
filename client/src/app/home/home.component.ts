83b7d8f50f301cf1ac321bae79e0ce620d220f60
Valerii2575 <valerii.osypenko@outlook.com>
2023-05-11 10:19:39 +0200

    update regicter component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -29,6 +29,10 @@ export class HomeComponent implements OnInit {
   registerToggle(){
     this.registerMode = !this.registerMode;
   }
+
+  cancelRegisterMode(event: boolean){
+    this.registerMode = event;
+  }
 }
 
 
